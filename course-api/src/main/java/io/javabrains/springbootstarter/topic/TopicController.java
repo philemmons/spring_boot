9c988fef7c0c49f856f9c0b10a7680fc3c22219c
philemmons <pemmons@csumb.edu>
2021-09-01 01:09:44 -0700

    review video 17, complete 18-20

--- course-api/src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
+++ course-api/src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -1,19 +1,20 @@
 package io.javabrains.springbootstarter.topic;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
 	
-	//  instance of singleton and inject it
-	@Autowired
+	//  instance of singleton and inject it to this class.
+	@Autowired // declare the dependency
 	private TopicService topicService;
-	
 	
 	//  remember to MAP the request and response
 	//  going to return a LIST of objects
@@ -22,5 +23,43 @@ public class TopicController {
 	public List<Topic> getAllTopics() {
 		return topicService.getAllTopics();//  http response
 	}
-
+	
+	//  Parenthesis create a variable token for Spring
+	@RequestMapping("/topics/{id}")
+	
+	//  whatever the path variable at 'foo', pass String id into it. 
+	//  this is not recommended
+	//  @RequestMapping("/topics/{foo}")
+	//  public Topic getTopic(@PathVariable("foo") String id) {
+	
+	//  path variable maps the id from the request, to the method parameter
+	public Topic getTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
+	}
+	
+	//  specify the method, we don't want to use the GET method
+	//  add the value property
+	//  map this method to any request that's a POST on slash topics.
+	//  get the post BODY, convert it into topic instance
+	//  we need to edit the POST body!
+	@RequestMapping(method=RequestMethod.POST, value="/topics")
+	//  Add @RequestBody with the Spring framework web bind annotation
+	//  Your request payload is going to contain a JSON representation of this topic instance
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
+	}
+	
+	//  change the POST method to PUT, add the {id} and PathVariable
+	//  create updateTopic method
+	//  same key:value on Postman - Content-Type:application/json
+	@RequestMapping(method=RequestMethod.PUT, value="/topics/{id}")
+	public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+		topicService.updateTopic(id, topic);
+	}
+	
+	@RequestMapping(method=RequestMethod.DELETE, value="/topics/{id}")
+	public void deleteTopic(@PathVariable String id) {
+		topicService.deleteTopic(id);
+	}
+	
 }
--- course-api/src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
+++ course-api/src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -1,23 +1,55 @@
 package io.javabrains.springbootstarter.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-// business service - typically a SINGLETON !!! (ONLY one copy)
+// business service - typically SINGLETON !!! (ONLY one instance)
 
-@Service //sterotypical business service
+@Service //Stereotypical business service
 public class TopicService {
 	
-	//  static list
-	List<Topic> topics = Arrays.asList( // creates List inline
+	//  static list, change to mutable list to POST(create) new topic via ArrayList<>()...
+	private List<Topic> topics = new ArrayList<>(Arrays.asList( // creates List in-line
 			new Topic("spring", "Spring Framework", "SF description"),
 			new Topic("java", "Core Java", "CJ description"),
 			new Topic("javascript", "JavaScript", "JS description")
-			);
+			));
 	
 	public List<Topic> getAllTopics(){
 		return topics;
 	}
+	
+	public Topic getTopic(String id) {
+		//  lambda expression filter
+		//  for each id, compare the id(parameter), find the first instance, returns it
+		//  returns one topic object - in a JSON format
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+
+	public void updateTopic(String id, Topic topic) {
+		Topic t;
+		int s = topics.size();
+		for(int i= 0; i< s; i++) {
+			t= topics.get(i);
+			if(t.getId().equals(id)) {
+				topics.set(i, topic);
+			}
+		}
+	}
+
+	
+	public void deleteTopic(String id) {
+		//  removes all topics in the list where the predicate is true
+		//  for each item in topics,  .removeIf(predicate) is true
+		topics.removeIf(t -> t.getId().equals(id));
+	}
+
+
 }
